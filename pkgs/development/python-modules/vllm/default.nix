d10673824d4ba09e36a9ec1bb1fc8ddbfb4464af
cfhammill <christopher.hammill@unityhealth.to>
2024-07-18 16:19:58 -0400

    python3Packages.vllm: 0.3.3->0.5.2

--- pkgs/development/python-modules/vllm/default.nix
+++ pkgs/development/python-modules/vllm/default.nix
@@ -1,9 +1,13 @@
 {
   lib,
+  stdenv,
+  python,
   buildPythonPackage,
+  pythonRelaxDepsHook,
   fetchFromGitHub,
   which,
   ninja,
+  cmake,
   packaging,
   setuptools,
   torch,
@@ -22,6 +26,13 @@
   pydantic,
   aioprometheus,
   pynvml,
+  openai,
+  pyzmq,
+  tiktoken,
+  torchvision,
+  py-cpuinfo,
+  lm-format-enforcer,
+  prometheus-fastapi-instrumentator,
   cupy,
   writeShellScript,
 
@@ -35,16 +46,26 @@
   gpuTargets ? [ ],
 }:
 
+let
+  stdenv_pkg = stdenv;
+  cutlass = fetchFromGitHub {
+    owner = "NVIDIA";
+    repo = "cutlass";
+    rev = "v3.5.0";
+    sha256 = "sha256-D/s7eYsa5l/mfx73tE4mnFcTQdYqGmXa9d9TCryw4e4=";
+  };
+in
+
 buildPythonPackage rec {
   pname = "vllm";
-  version = "0.3.3";
+  version = "0.5.2";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "vllm-project";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-LU5pCPVv+Ws9dL8oWL1sJGzwQKI1IFk2A1I6TP9gXL4=";
+    hash = "sha256-CUyYTmV1+Clt/npBVJlm0lhqb4vDD8MYABnw62fZVVI=";
   };
 
   # Otherwise it tries to enumerate host supported ROCM gfx archs, and that is not possible due to sandboxing.
@@ -57,51 +78,42 @@ buildPythonPackage rec {
   # https://github.com/vllm-project/vllm/pull/2845/commits/34a0ad7f9bb7880c0daa2992d700df3e01e91363
   #
   # hipcc --version works badly on NixOS due to unresolved paths.
-  # Unclear why pythonRelaxDeps doesn't work here, but on last attempt, it didn't.
-  postPatch =
-    ''
-      substituteInPlace requirements.txt \
-        --replace "xformers == 0.0.23.post1" "xformers"
-      substituteInPlace requirements.txt \
-        --replace "cupy-cuda12x == 12.1.0" "cupy"
-      substituteInPlace requirements-build.txt \
-        --replace "torch==2.1.2" "torch"
-      substituteInPlace pyproject.toml \
-        --replace "torch == 2.1.2" "torch"
-      substituteInPlace requirements.txt \
-        --replace "torch == 2.1.2" "torch"
-    ''
-    + lib.optionalString rocmSupport ''
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace 'cmake_args = [' 'cmake_args = ["-DFETCHCONTENT_SOURCE_DIR_CUTLASS=${cutlass}",'
+    '' +
+    lib.optionalString rocmSupport ''
       substituteInPlace setup.py \
         --replace "'hipcc', '--version'" "'${writeShellScript "hipcc-version-stub" "echo HIP version: 0.0"}'"
     '';
 
-  preBuild =
-    lib.optionalString cudaSupport ''
-      export CUDA_HOME=${cudaPackages.cuda_nvcc}
-    ''
-    + lib.optionalString rocmSupport ''
+  preBuild = lib.optionalString rocmSupport ''
       export ROCM_HOME=${rocmPackages.clr}
       export PATH=$PATH:${rocmPackages.hipcc}
+    '' + lib.optionalString cudaSupport ''
+      export CUDA_HOME=${cudaPackages.cudatoolkit}
     '';
 
   nativeBuildInputs = [
-    ninja
     packaging
     setuptools
     torch
     wheel
     which
+    cmake
+    ninja
+    pythonRelaxDepsHook #unclear why this is needed, but it seems to be
   ] ++ lib.optionals rocmSupport [ rocmPackages.hipcc ];
 
   buildInputs =
     (lib.optionals cudaSupport (
       with cudaPackages;
       [
-        cuda_cudart # cuda_runtime.h, -lcudart
+        cudatoolkit
+        cuda_cudart.dev # cuda_runtime.h, -lcudart
+        cuda_cudart.lib
         cuda_cccl.dev # <thrust/*>
         libcusparse.dev # cusparse.h
-        libcublas.dev # cublas_v2.h
         libcusolver # cusolverDn.h
       ]
     ))
@@ -132,6 +144,13 @@ buildPythonPackage rec {
       uvicorn
       pydantic
       aioprometheus
+      openai
+      pyzmq
+      tiktoken
+      torchvision
+      py-cpuinfo
+      lm-format-enforcer
+      prometheus-fastapi-instrumentator
     ]
     ++ uvicorn.optional-dependencies.standard
     ++ aioprometheus.optional-dependencies.starlette
@@ -139,6 +158,10 @@ buildPythonPackage rec {
       pynvml
       cupy
     ];
+
+  dontUseCmakeConfigure=true;
+
+  pythonRelaxDeps = true;
 
   stdenv = if cudaSupport then cudaPackages.backendStdenv else stdenv;
 
