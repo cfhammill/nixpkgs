a19a1ce94f63de8c0dda45f51e976b080f3a3eff
cfhammill <christopher.hammill@unityhealth.to>
2024-08-15 11:39:07 -0400

    rsconnect-python: init at 1.2.4 (patched)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13607,6 +13607,8 @@ self: super: with self; {
 
   rsa = callPackage ../development/python-modules/rsa { };
 
+  rsconnect-python = callPackage ../development/python-modules/rsconnect-python { };
+
   rsskey = callPackage ../development/python-modules/rsskey { };
 
   rst2ansi = callPackage ../development/python-modules/rst2ansi { };
